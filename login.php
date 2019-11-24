e3ea71abda4e684b202009bef9373e6b9750d87d
vinaboy0710 <57781840+vinaboy0710@users.noreply.github.com>
2019-11-24 12:25:00 +0700

    Update login.php

--- login.php
+++ login.php
@@ -1,12 +1,12 @@
 $fb = new Facebook\Facebook([
-  'app_id' => '{app-id}', // Replace {app-id} with your app id
-  'app_secret' => '{app-secret}',
-  'default_graph_version' => 'v2.2',
+  '2632212860169522' => '{app-id}', // Replace {app-id} with your app id
+  '133eead69318cef1a838274422606079' => '{app-secret}',
+  'default_graph_version' => 'v5.0',
   ]);
 
 $helper = $fb->getRedirectLoginHelper();
 
 $permissions = ['email']; // Optional permissions
-$loginUrl = $helper->getLoginUrl('https://example.com/fb-callback.php', $permissions);
+$loginUrl = $helper->getLoginUrl('https://github.com/vinaboy0710/fb-callback/blob/master/fb-callback.php', $permissions);
 
 echo '<a href="' . htmlspecialchars($loginUrl) . '">Log in with Facebook!</a>';
